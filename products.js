8ca4d92aba863e75265c83c8a91cfe11d1c9f33d
Jung-Yi, Tsai <jungyitsai@gmail.com>
2020-07-19 16:50:48 +0800

    [modify] let products component modularize

---
+++ products.js
@@ -0,0 +1,44 @@
+export default {
+    template: `
+        <table class="table mt-4">
+            <thead>
+            <tr>
+                <th scope="col">分類</th>
+                <th scope="col">產品名稱</th>
+                <th scope="col">原價</th>
+                <th scope="col">售價</th>
+                <th scope="col">是否啟用</th>
+                <th scope="col">編輯</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, key) in products" :key="item.title">
+                    <td>{{ item.category }}</td>
+                    <td>{{ item.title }}</td>
+                    <td>{{ item.origin_price }}</td>
+                    <td>{{ item.price }}</td>
+                    <td>
+                        <span v-if="item.enabled" class="text-success">啟用</span>
+                        <span v-else>未啟用</span>
+                    </td>
+                    <td>
+                        <!-- 合再一起的元件，通常都是 xxx-group ( ex: btn-group, list-group ) -->
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-outline-primary" @click="openModal('update', item)">編輯</button>
+                            <button type="button" class="btn btn-outline-danger"
+                                @click="openModal('delete', item)">刪除</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>`,
+    props: ['products'],
+    data() {
+        return {
+            tempProduct: {},
+        }
+    },
+    methods: {
+
+    }
+}
